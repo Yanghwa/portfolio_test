4524b6e90f48544a10af6d50d505a1a1143528a6
Yanghwa <kellerj87@hotmail.com>
2017-01-31 19:44:31 -0500

    first version of site

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About', extara : 'Junghwan Yang\'s about' });
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('index', { title: 'Blog', extara : 'Junghwan Yang\'s products' });
+});
+
+// router.get('/services', function(req, res, next) {
+//   res.render('index', { title: 'Services', extara : 'Junghwan Yang\'s services' });
+// });
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact', extara : 'Junghwan Yang\'s contact' });
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', owner : 'Junghwan Yang' });
+  res.render('index', { title: 'Home', extara : 'Junghwan Yang\'s page' });
 });
 
 module.exports = router;
