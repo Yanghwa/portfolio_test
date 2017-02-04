8702326e8ea4e40d7b352d219a1289da73a87404
Yanghwa <kellerj87@hotmail.com>
2017-02-03 20:29:59 -0500

    setting the basic structure-not completed

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,13 @@ router.get('/blog', function(req, res, next) {
   res.render('./partials/blog', { title: 'Blog', extara : 'Junghwan Yang\'s Blog' });
 });
 
-// router.get('/services', function(req, res, next) {
-//   res.render('index', { title: 'Services', extara : 'Junghwan Yang\'s services' });
-// });
+router.get('/projects', function(req, res, next) {
+  res.render('./partials/projects', { title: 'Projects', extara : 'Junghwan Yang\'s Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('./partials/services', { title: 'Services', extara : 'Junghwan Yang\'s Services' });
+});
 
 router.get('/contact', function(req, res, next) {
   res.render('./partials/contact', { title: 'Contact', extara : 'Junghwan Yang\'s Contact' });
