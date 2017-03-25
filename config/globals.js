d28ec38900edcc3a3e6f7cc3397abe7f753064dd
Yanghwa <kellerj87@hotmail.com>
2017-03-25 18:54:20 -0400

    readme added

--- config/globals.js
+++ config/globals.js
@@ -5,6 +5,6 @@
 //using Read Only account.
 //exports db connection
 module.exports = {
-	db: 'mongodb://YanghwaRead:nwjdfmd@ds145639.mlab.com:45639/junghwandb'
+	//db: 'mongodb://YanghwaRead:nwjdfmd@ds145639.mlab.com:45639/junghwandb'
 	// db: 'mongodb://localhost:27017'
 };
